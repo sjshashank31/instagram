40929d1fcc7618dd4f77383722d51a635850bc3a
shashank jain <54624059+sjshashank31@users.noreply.github.com>
2020-07-09 20:31:15 +0530

    Create phone.py

---
+++ phone.py
@@ -0,0 +1,19 @@
+import phonenumbers
+
+'''geocoder: to know the specific
+location to that phone number
+carrier: to know the name of
+service provider of that phone number'''
+
+from phonenumbers import carrier
+from phonenumbers import geocoder
+
+phone_number = phonenumbers.parse("+918696558836")
+
+# this will print the service provider name
+print(carrier.name_for_number(phone_number,
+                              'en'))
+
+# this will print the country name
+print(geocoder.description_for_number(phone_number,
+                                      'en'))
